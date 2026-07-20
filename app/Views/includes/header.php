afdc967e99d5048a3c81343b72a76bbdcbe9b163
RAOYo <yoannrah12@outlook.com>
2026-07-20 11:42:34 +0300

    modifier header

--- app/Views/includes/header.php
+++ app/Views/includes/header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?= isset($title) ? $title . ' — TanaPay' : 'TanaPay — Espace opérateur' ?></title>
+  <title><?= isset($title) ? $title . 'ValsMobile' : 'ValsMobile' ?></title>
   <link rel="stylesheet" href="<?= base_url('template/design-system.css') ?>">
 </head>
 
